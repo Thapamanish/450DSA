bf39efe63c2f92c4d18176db292d3cc529851cb4
Manish Thapa <iammanish041@gmail.com>
2021-07-14 12:55:55 +0545

    TCS question

---
+++ TCS/Candies.py
@@ -0,0 +1,22 @@
+'''There is a JAR full of candies for sale at a mall counter. JAR has the capacity N, that is JAR can contain maximum N candies when JAR is full. At any point of time. JAR can have M number of Candies where M<=N. Candies are served to the customers. JAR is never remain empty as when last k candies are left. JAR if refilled with new candies in such a way that JAR get full.
+Write a code to implement above scenario. Display JAR at counter with available number of candies. Input should be the number of candies one customer can order at point of time. Update the JAR after each purchase and display JAR at Counter.
+
+Output should give number of Candies sold and updated number of Candies in JAR.
+
+If Input is more than candies in JAR, return: “INVALID INPUT”
+
+Given, 
+
+N=10, where N is NUMBER OF CANDIES AVAILABLE
+
+K =< 5, where k is number of minimum candies that must be inside JAR ever.'''
+
+
+N = 10
+user_input = int(input())
+if user_input in range(1, 6):
+   print("No. of Candies sold:", user_input)
+   print("No. of Candies remaining:", N - user_input)
+else:
+   print("INVALID INPUT")
+   print("No. of Candies remaining:", N)
