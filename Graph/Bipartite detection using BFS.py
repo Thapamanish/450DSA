373c8d4940c8f796222dd0180fb00ecfefb2eaa5
Manish Thapa <iammanish041@gmail.com>
2021-08-09 19:26:40 +0530

    add graph algorithms

---
+++ Graph/Bipartite detection using BFS.py
@@ -0,0 +1,61 @@
+from collections import defaultdict
+
+class Graph:
+    def __init__(self):
+        self.graph = defaultdict(list)
+
+    def addEdge(self, u, v):
+        self.graph[u].append(v)
+        self.graph[v].append(u)
+
+    def bipartiteUtil(self, v, color):
+
+        queue = []
+        queue.append(v)
+
+        while queue:
+            s = queue.pop(0)
+            for neighbor in self.graph[s]:
+                if color[neighbor] == -1:
+                    queue.append(neighbor)
+                    color[neighbor] = 1 - color[s]
+                elif color[neighbor] == color[s]:
+                    return False
+        return True
+
+    def bipartite(self):
+        color = [-1] * len(self.graph)
+        for vertex in list(self.graph):
+            if color[vertex] == -1:
+                color[vertex] = 0
+                if self.bipartiteUtil(vertex, color):
+                    return True
+        return False
+
+g = Graph() 
+g.addEdge(0, 1)
+g.addEdge(1, 2)
+g.addEdge(2, 3)
+
+
+  
+if g.bipartite(): 
+    print("Graph is bipartite")
+else : 
+    print ("Graph is not bipartite")
+
+
+
+g1 = Graph() 
+g1.addEdge(0,1) 
+g1.addEdge(1,2) 
+g1.addEdge(2,3)
+g1.addEdge(3,4)
+g1.addEdge(4,5)
+g1.addEdge(5,3)
+
+
+if g1.bipartite(): 
+    print("Graph is bipartite")
+else : 
+    print ("Graph is not bipartite")
